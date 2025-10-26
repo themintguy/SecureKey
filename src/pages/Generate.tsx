e7af99552d81f546c98f8c13325a306036d59fd2
hanaaden <hanaadamabdi@gmail.com>
2025-10-26 18:04:56 +0300

    touches

--- src/pages/Generate.tsx
+++ src/pages/Generate.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 function Generate() {
   const [UpperCase, setUppercase] = useState(false)
--- src/pages/TestimonialCard.tsx
+++ src/pages/TestimonialCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { FaStar, FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
 interface TestimonailCardProps {
